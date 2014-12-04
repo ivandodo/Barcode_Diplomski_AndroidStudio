6c018134e555ef876a7d37cc33bd43bf6169494a
Ivan Radojevic <ivandodo@gmail.com>
2014-12-04 14:36:15 +0100

    preimenovani paketi

--- Barcode Test/src/com/example/barcode/test/BarcodeConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.barcode.test;
-
-
-public class BarcodeConstants {
-	
-	public static final String CLASS_SOAP_PRIMITIVE = "org.ksoap2.serialization.SoapPrimitive";
-	public static final String CLASS_SOAP_OBJECT = "org.ksoap2.serialization.SoapObject";
-
-}
